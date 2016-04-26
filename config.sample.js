90707008b967d2671a32e4d7ca2101ed0a973fc5
mstan <1379tech@gmail.com>
2016-04-26 00:47:40 -0700

    Add config.sample.js file. Clean up documentation

--- config.sample.js
+++ config.sample.js
@@ -1,8 +1,8 @@
 data = {
   "botAccountName": "<USERNAME>",
-  "botAccountPwd": "<PASSWORD",
+  "botAccountPwd": "<PASSWORD>",
   "botSharedSecret": "<BOT SHARED SECRET>", //Must be generated secret from 2factor
-  "ownerName": "", //Arbitray. What others will see in bot messages
+  "ownerName": "Gamemaster", //Arbitray. What others will see in bot messages
   "ownerSteamID": "STEAM_0:0:27384306", //To set your account as owner and give admin
   "ownerSteamURLID": "76561198015034340" //Your steam profile URL ID
 
