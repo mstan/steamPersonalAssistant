793e80daa16c0a0b5d8aee6c6964c5cf6d11275b
mstan <1379tech@gmail.com>
2016-04-26 00:45:53 -0700

    Remove unwanted lib file. Individualize error messages. Externalize & Compact Error messages. Add proper help commands. Change message handlers syntax.

--- resource.js
+++ resource.js
@@ -2,31 +2,60 @@ var config = require('./config.js');
 
 //admin panel messages
 var adminPanel = {
-    "invalidStatus": 'ERROR: Invalid status',
+    //Status
+    "invalidStatus":        'ERROR: Invalid status',
+    "noStatusGiven":        'ERROR: No Status Given',
+    //Send Message
+    "invalidFormatSendMsg": 'ERROR: Invalid userID or message format',
+    "noMsgToSend":          'ERROR: No message to send',
+    //Add User
+    "noIDSpecified":        'ERROR: No ID given',
+    "invalidSteamID":       'ERROR: Invalid Steam ID format',
+    //Set Name
+    "noNameGiven":          'ERROR: No name given',
+    //Play game
+    "noGameOrID":           'ERROR: No game or game ID given'
 }
 
 //user panel messages
-var userPanel = {
-    "defaultMessage": 'Welcome to ' + config.ownerName + '\'s chat bot! What would you like me to do?'
+var userPanel = {  
+    "commandNotFound": 'ERROR: Command not found',      
+    //For the default fall through case
+    "defaultMessage": 'Welcome to ' + config.ownerName + '\'s chat bot! \n \n' +
+                      'Sorry! I didn\'t quite know what to do with your message, ' +
+                      'but if you type "help", I\'ll give you a list of commands that I know! \n \n' +
+                      'You can also type help: <COMMAND> to get more information about that command!'
 }
 
 //help commands
 var userCommandsHelp = {
-    "play game": "Make this bot play a game using appid or non-steam game (Non-steam) \n" + 
-                 "[USAGE] play game: <APPID>/<NAME> \n" +
-                 "[EXAMPLE] play game: 440 (Play Team Fortress 2) \n" +
-                 "[EXAMPLE] play game: Minecraft",
-    "test": "help"
-}
+    "add user":     'Add a user by their steam ID \n \n' +
+                    '[USAGE] add user: <STEAM ID> \n \n' +
+                    '[EXAMPLE] add user: STEAM_0:0:27384306',
 
-var registeredUsers = {
-    "Gamemaster": '76561198015034340',
-    "User": '1'
+    "play game":    'Make this bot play a game using appid or non-steam game \n \n' + 
+                    '[USAGE] play game: <APPID>/<NAME> \n \n' +
+                    '[EXAMPLE] play game: 440 (Play Team Fortress 2) \n' +
+                    '[EXAMPLE] play game: Minecraft',
+
+
+    "send message":  'Send a message to the specified steam user \n \n' +
+                     '[USAGE] send message: <STEAM URL ID>; <MESSAGE> \n \n' +
+                     '[EXAMPLE] send message: 76561198015034340; Hello!',
+
+    "set name":      'Change this bot\'s name \n \n' +
+                     '[USAGE] set name: <NAME> \n \n' +
+                     '[EXAMPLE] set name: Gamemaster\'s Bot',
+
+    "set status":    'Change this bot\'s status' +
+                     'Available statuses: Online, Busy, Looking To Play, Looking To Trade, Snooze, Offline*' +
+                     'NOTE: Offline is full sign out. This is not equivalent to appearing offline \n \n' +
+                     '[USAGE] set status: <STATUS> \n \n' +
+                     '[EXAMPLE] set status: busy'
 }
 
 module.exports = {
     adminPanel: adminPanel,
     userPanel: userPanel,
-    userCommandsHelp: userCommandsHelp,
-    registeredUsers: registeredUsers
+    userCommandsHelp: userCommandsHelp
 }
